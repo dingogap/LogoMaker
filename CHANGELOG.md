33be7fd390ab56595b1e48cab862a46244d3e303
Peter Medbury <peter.medbury@dingogap.net.au>
2023-07-28 16:23:58 +1000

    added code to ask questions and write answers to file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,5 @@
 2. Created Folder Structure
 3. Initialised npm & installed inquirer & jest packages
 4. Created questions.js in the lib folder and created the questions array
+5. Created buildLogo.js in the lib folder to hold code to control logo build
+6. Add code to ask questions and write the Logo to file
--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const questions = require('./lib/questions.js');
+const buildLogo = require('./lib/buildLogo.js');
+const fileName = "./examples/logo.svg"
+
+// Create file filename & write data to it
+// Log success to console
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} created successfully`);
+        }
+    });
+}
+
+// Intialise Processing
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            return buildLogo(answers);
+        })
+        .then((data) => {
+            writeToFile(fileName, data);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+// Function call to initialize app
+init();
