d089f8357ae09b1b12b2e6b85c9e0c9f4d78bab0
Peter Medbury <peter.medbury@dingogap.net.au>
2023-07-28 16:27:52 +1000

    added comments to code

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
+// Reference Packages
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+// Include external js files
 const questions = require('./lib/questions.js');
 const buildLogo = require('./lib/buildLogo.js');
+
+// Declare variables
 const fileName = "./examples/logo.svg"
 
-// Create file filename & write data to it
-// Log success to console
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            console.error(err);
-        } else {
-            console.log(`${fileName} created successfully`);
-        }
-    });
-}
+// Start Processing
+init();
 
-// Intialise Processing
+// Intialise Processing - Ask Questions & when answered build logo
 function init() {
     inquirer
         .prompt(questions)
@@ -31,5 +27,14 @@ function init() {
         });
 }
 
-// Function call to initialize app
-init();
+// Create file filename & write logo data to it
+// Log success to console
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`${fileName} created successfully`);
+        }
+    });
+}
